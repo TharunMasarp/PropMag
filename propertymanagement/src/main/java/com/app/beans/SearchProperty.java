0f1fc6eec8480c629e882c81a71f62da7d31c0c6
User <User@Lenovo>
2019-02-06 14:54:41 +0530

    Home

---
+++ propertymanagement/src/main/java/com/app/beans/SearchProperty.java
@@ -0,0 +1,14 @@
+package com.app.beans;
+
+public class SearchProperty {
+	private String city;
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+}
